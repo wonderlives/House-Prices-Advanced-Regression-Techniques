373342c0aa4f0033f73afa78fb1a05bd0dd7565d
Andrew <adodd202@gmail.com>
2018-03-02 14:53:33 -0500

    Cleaning up my files

--- housing_main.py
+++ housing_main.py
@@ -3,6 +3,11 @@ from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import mean_squared_error
 
 
+class meanStacker():
+	def __init__(X, y):
+		# Inputs:
+		#	- X: this is the matrix 
+
 
 
 
